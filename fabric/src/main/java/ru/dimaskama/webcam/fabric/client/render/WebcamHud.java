0d249416cd943756a4e1a938f68e725bd088ec39
Mynam <mynameisiphone10@gmail.com>
2025-07-10 03:29:29 +0500

    - Added more informative icons - Fix menu open ability while not authenticated to webcam server
    # Conflicts:
    #	changelog.md
    #	fabric/src/main/java/ru/dimaskama/webcam/fabric/client/render/WebcamHud.java
    
    # Conflicts:
    #	changelog.md

--- fabric/src/main/java/ru/dimaskama/webcam/fabric/client/render/WebcamHud.java
+++ fabric/src/main/java/ru/dimaskama/webcam/fabric/client/render/WebcamHud.java
@@ -8,6 +8,7 @@ import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.resources.ResourceLocation;
 import ru.dimaskama.webcam.fabric.WebcamFabric;
+import ru.dimaskama.webcam.fabric.client.WebcamClient;
 import ru.dimaskama.webcam.fabric.client.WebcamFabricClient;
 import ru.dimaskama.webcam.fabric.client.Webcams;
 
@@ -16,12 +17,26 @@ public class WebcamHud {
     public static final boolean IS_VOICECHAT_LOADED = FabricLoader.getInstance().isModLoaded("voicechat");
     private static final ResourceLocation WEBCAM_SPRITE = WebcamFabric.id("webcam");
     private static final ResourceLocation WEBCAM_DISABLED_SPRITE = WebcamFabric.id("webcam_disabled");
+    private static final ResourceLocation WEBCAM_NO_CONNECTION_SPRITE = WebcamFabric.id("webcam_no_connection");
+    private static final ResourceLocation WEBCAM_CONNECTING_SPRITE = WebcamFabric.id("webcam_connecting");
 
     public static void drawHud(GuiGraphics guiGraphics, DeltaTracker deltaTracker) {
         if (WebcamFabricClient.CONFIG.getData().showIcons()) {
             Window window = Minecraft.getInstance().getWindow();
             int height = window.getGuiScaledHeight();
-            ResourceLocation sprite = Webcams.isCapturing() ? WEBCAM_SPRITE : WEBCAM_DISABLED_SPRITE;
+            ResourceLocation sprite;
+            if (Webcams.isCapturing()) {
+                sprite = WEBCAM_SPRITE;
+            } else {
+                WebcamClient client = WebcamClient.getInstance();
+                if (client == null || client.isClosed()) {
+                    sprite = WEBCAM_NO_CONNECTION_SPRITE;
+                } else if (!client.isAuthenticated()) {
+                    sprite = WEBCAM_CONNECTING_SPRITE;
+                } else {
+                    sprite = WEBCAM_DISABLED_SPRITE;
+                }
+            }
             guiGraphics.blitSprite(RenderType::guiTextured, sprite, IS_VOICECHAT_LOADED ? 32 : 16, height - 32, 16, 16);
         }
     }
